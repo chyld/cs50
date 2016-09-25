91f1b6def0f68139d83144dbb36d8358413f5aa4
Chyld Medford <chyld.medford@gmail.com>
2016-09-24 23:34:21 -0700

    merge sort + binary search

--- set03/find.js
+++ set03/find.js
@@ -1,18 +1,34 @@
 const needle = parseInt(process.argv[2]);
-let nums = [];
 
-for(let i = 0; i < 10; i++){
-  const num = Math.floor(Math.random() * 10);
-  nums.push(num);
+let nums = Array.apply(null, Array(10)).map(n => {
+  return Math.floor(Math.random() * 10);
+});
+
+console.log('needle:', needle, 'pre:', nums, 'post:', sort(nums), 'found:', search(needle, sort(nums)));
+
+function search(needle, haystack){
+  if(!haystack.length){
+    return false;
+  }
+
+  if(haystack.length === 1){
+    return needle === haystack[0];
+  }
+
+  const half = Math.floor(haystack.length / 2);
+
+  if(haystack[half] === needle){
+    return true;
+  }
+
+  if(haystack[half] > needle){
+    return search(needle, haystack.slice(0, half));
+  }else{
+    return search(needle, haystack.slice(half));
+  }
 }
 
-nums = [6,4,1,7,-2,1,3,3,3,-4,9,2,1,7];
-
-const good = sort(nums, 0);
-console.log('good:', good);
-
-function sort(list, x){
-  console.log('fn:', list, 'depth:', x);
+function sort(list){
   if(list.length < 2){
     return list;
   }else{
@@ -20,26 +36,22 @@ function sort(list, x){
     const left = list.slice(0, half);
     const right = list.slice(half);
 
-    const l1 = sort(left, x + 1);
-    const l2 = sort(right, x + 1);
+    const l1 = sort(left);
+    const l2 = sort(right);
     return merge(l1, l2);
   }
 }
 
 function merge(l1, l2){
-  const temp = [];
-  while(l1.length || l2.length){
-    if(l1.length && l2.length){
-      if(l1[0] < l2[0]){
-        temp.push(l1.shift());
-      }else{
-        temp.push(l2.shift());
-      }
-    }else if(!l2.length){
+  let temp = [];
+  while(true){
+    if(!l1.length || !l2.length){
+      return [...temp, ...l1, ...l2];
+    }
+    if(l1[0] < l2[0]){
       temp.push(l1.shift());
     }else{
       temp.push(l2.shift());
     }
   }
-  return temp;
 }
